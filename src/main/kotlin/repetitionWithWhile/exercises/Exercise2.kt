25ca26e4fd6c344898ac904cd398f6ae86f84249
Wybson Santana <wybson@hotmail.com>
2022-04-22 21:57:18 -0300

    Create repetition with while examples

---
+++ src/main/kotlin/repetitionWithWhile/exercises/Exercise2.kt
@@ -0,0 +1,20 @@
+package repetitionWithWhile.exercises
+
+fun sum(number: Int): Int {
+    var sum = 0
+    var i = 0
+    while (i <= number) {
+        sum += i
+        i++
+    }
+    return sum
+}
+
+fun main() {
+    println(sum(10))
+}
+
+/*
+Output:
+55
+*/
---
+++ src/main/kotlin/repetitionWithWhile/exercises/Exercise3.kt
@@ -0,0 +1,22 @@
+package repetitionWithWhile.exercises
+
+fun sumOfEven(number: Int): Int {
+    var result = 0
+    var i = 0
+    while (i <= number) {
+        if (i % 2 == 0) {
+            result += i
+        }
+        i++
+    }
+    return result
+}
+
+fun main() {
+    println(sumOfEven(10))
+}
+
+/*
+Output:
+30
+*/
